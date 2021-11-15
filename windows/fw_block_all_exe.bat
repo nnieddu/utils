804ee3a0c2395f5d57c213c89de0da51b0f60e81
nnieddu <nicolas.nieddu69@gmail.com>
2021-11-15 09:02:39 +0100

    test

---
+++ windows/fw_block_all_exe.bat
@@ -0,0 +1,6 @@
+for %%G in ("C:\Program Files (x86)\[FOLDER CONTAINING EXECUTABLES]\*.exe") do (
+
+netsh advfirewall firewall add rule name="Blocked With Batchfile %%G" dir=in action=block program="%%G" enable=yes profile=any
+netsh advfirewall firewall add rule name="Blocked With Batchfile %%G" dir=out action=block program="%%G" enable=yes profile=any
+
+)
