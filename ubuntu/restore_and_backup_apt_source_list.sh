9379b2545cfcef76065fc8826f0d77f5f708a7ae
nnieddu <nicolas.nieddu@free.fr>
2022-01-26 14:22:18 +0100

    cleaner

--- ubuntu/restore_and_backup_apt_source_list.sh
+++ ubuntu/restore_and_backup_apt_source_list.sh
@@ -1,8 +1,8 @@
 #Ubuntu
 sudo mkdir -p /etc/apt/backup_srcs/ 
-wget https://gist.githubusercontent.com/ninieddu/utils/ubuntu/backup/sources.list
-sudo mv -b /etc/apt/sources.list /etc/apt/backup_srcs/sources.list.bak
-sudo mv -fb sources.list /etc/apt/sources.list 
+wget https://raw.githubusercontent.com/nnieddu/utils/main/ubuntu/backup/sources.list
+sudo mv -bi /etc/apt/sources.list /etc/apt/backup_srcs/
+sudo mv -f sources.list /etc/apt/
 # Update to same host os/ubuntu version : 
 sudo sed -i "s/focal/$(lsb_release -c -s)/" /etc/apt/sources.list
 sudo add-apt-repository universe
