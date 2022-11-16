0c98997aa7d7685371acc16f840d9e37d8c9ab74
nnieddu <33942156+nnieddu@users.noreply.github.com>
2022-11-16 09:58:47 +0100

    Update deployReactPortfolio.bat

--- windows/deployReactPortfolio.bat
+++ windows/deployReactPortfolio.bat
@@ -1,5 +1,25 @@
+```
+  "scripts": {
+    "mybuild": "npm run sass && npm run prefix && deployScript.bat",
+    "sass": "sass ./src/styles/App.scss:./src/styles/App.css --style compressed",
+    "prefix": "postcss ./src/styles/App.css --use autoprefixer -d ./src/styles/"
+    },
+    "browserslist": {
+      "production": [
+      "last 4 versions",
+      ">0.2%",
+      "not dead",
+      "not op_mini all"
+    ],
+    "development": [
+      "last 4 chrome version",
+      "last 4 firefox version",
+      "last 4 safari version"
+    ]
+  }
+```
+  
 @echo off
-@REM npm run build
 echo "--------------------------------"
 echo "--------------------------------"
 echo "--------- FIN DU BUILD ---------"
@@ -7,14 +27,12 @@ echo "--------------------------------"
 echo "--------------------------------"
 echo ""
 echo ""
-git pull
+@REM git pull
 git add *
 git commit -m %1
 git push
 echo "--------------------------------"
-echo "--------------------------------"
 echo "--------- PUSH RAW CODE --------"
-echo "--------------------------------"
 echo "--------------------------------"
 echo ""
 echo ""
@@ -27,9 +45,7 @@ cd "nnieddu.github.io\"
 echo ""
 echo ""
 echo "--------------------------------"
-echo "--------------------------------"
 echo "--------- FIN DE COPY ---------"
-echo "--------------------------------"
 echo "--------------------------------"
 echo ""
 echo ""
@@ -37,9 +53,7 @@ echo ""
 echo ""
 echo ""
 echo "--------------------------------"
-echo "--------------------------------"
 echo "--------- FIN DU PULL ---------"
-echo "--------------------------------"
 echo "--------------------------------"
 echo ""
 echo ""
@@ -49,7 +63,5 @@ git push
 echo ""
 echo ""
 echo "--------------------------------"
-echo "--------------------------------"
 echo "----- FIN DU DEPLOIEMENT -------"
-echo "--------------------------------"
 echo "--------------------------------"
