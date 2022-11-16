52e2ecd26760424d5834f26cd74a1ce02162c527
nnieddu <33942156+nnieddu@users.noreply.github.com>
2022-11-16 10:21:04 +0100

    Update deployReactPortfolio.bat

--- windows/deployReactPortfolio.bat
+++ windows/deployReactPortfolio.bat
@@ -19,49 +19,64 @@
   }
 ```
   
-@echo off
-echo "--------------------------------"
-echo "--------------------------------"
-echo "--------- FIN DU BUILD ---------"
-echo "--------------------------------"
-echo "--------------------------------"
-echo ""
-echo ""
+@REM [100m !ESC![100m Black !ESC![0m
+@REM [101m !ESC![101m Red !ESC![0m
+@REM [102m !ESC![102m Green !ESC![0m
+@REM [103m !ESC![103m Yellow !ESC![0m
+@REM [104m !ESC![104m Blue !ESC![0m
+@REM [105m !ESC![105m Magenta !ESC![0m
+@REM [106m !ESC![106m Cyan !ESC![0m
+@REM [107m !ESC![107m White !ESC![0m
+
+@ECHO OFF
+SETLOCAL EnableExtensions DisableDelayedExpansion
+for /F %%a in ('echo prompt $E ^| cmd') do (
+  set "ESC=%%a"
+)
+SETLOCAL EnableDelayedExpansion
+
+echo:
+echo --------------------------------
+echo ---------!ESC![92m FIN DU BUILD !ESC![0m---------
+echo --------------------------------
+echo:
+echo:
 @REM git pull
 git add *
 git commit -m %1
 git push
-echo "--------------------------------"
-echo "--------- PUSH RAW CODE --------"
-echo "--------------------------------"
-echo ""
-echo ""
+echo --------------------------------
+echo --------- PUSH RAW CODE --------
+echo --------------------------------
+echo:
+echo:
 cd "nnieddu.github.io\"
 git rm -rf "static\"
 cd ..
 dir
 xcopy /E /Y "build\*" "nnieddu.github.io\."
 cd "nnieddu.github.io\"
-echo ""
-echo ""
-echo "--------------------------------"
-echo "--------- FIN DE COPY ---------"
-echo "--------------------------------"
-echo ""
-echo ""
+echo:
+echo:
+echo --------------------------------
+echo --------- FIN DE COPY ----------
+echo --------------------------------
+echo:
+echo:
 @REM git pull
-echo ""
-echo ""
-echo "--------------------------------"
-echo "--------- FIN DU PULL ---------"
-echo "--------------------------------"
-echo ""
-echo ""
+echo:
+echo:
+echo --------------------------------
+echo --------- FIN DU PULL ----------
+echo --------------------------------
+echo:
+echo:
 git add *
 git commit -m %1
 git push
-echo ""
-echo ""
-echo "--------------------------------"
-echo "----- FIN DU DEPLOIEMENT -------"
-echo "--------------------------------"
+echo:
+echo:
+echo -------------------------------
+echo ---- FIN DU DEPLOIEMENT -------
+echo -------------------------------
+echo:
